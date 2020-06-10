1fcdc7a4d2d5e2ba2f0a2c81c704c8773b631763
Lika <shelley.bass.li@gmail.com>
2020-06-10 22:54:16 +0300

    Checking a third-party utility for Enum recognition in the code

---
+++ Asp_Web_Learning/GamedevStorage/ExampleEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamedevStorage
+{
+    enum ShopObjectEnum
+    {
+        ShopObject1 = 2363,
+        ShopObject2 = 34563,
+        ShopObject3 = 54563,
+        ShopObject4 = 76576,
+        ShopObject5 = 7983
+    }
+}
